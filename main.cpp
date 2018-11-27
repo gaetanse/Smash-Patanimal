5ad59c63eaa8db12b44224ac53b61a6c395ba3d8
gaetan <gaetansdx@gmail.com>
2018-11-27 20:21:06 +0100

    Affichage/fenetre

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "librairies.h"
+#include "fenetre.h"
+
+int main(){
+
+    fenetre Window;
+
+    Window.GetCopieRenderWindow().setFramerateLimit(60);
+
+    while(Window.GetCopieRenderWindow().isOpen()){
+
+
+
+    }
+
+}
