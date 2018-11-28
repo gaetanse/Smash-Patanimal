4f1830b3a13ee62973448040a579a2dc8dbc1127
gaetan <gaetansdx@gmail.com>
2018-11-28 17:15:30 +0100

    ajout des rectangles rouges

--- jeu.cpp
+++ jeu.cpp
@@ -59,6 +59,9 @@ Window.RenderW().setView(jeuCamera);
 affichage_map();
 Window.RenderW().setView(Window.RenderW().getDefaultView());
 
+for(unsigned int b=0;b<rectangles.size();b++)
+    Window.RenderW().draw(rectangles.at(b));
+
 for(unsigned int a=0;a<texts.size();a++)
     Window.RenderW().draw(texts.at(a));
 
@@ -114,9 +117,10 @@ jeu::jeu()
         tileSet1.setTexture(tileSet1Texture);
 
     jeuCamera.reset(sf::FloatRect(0, 0, 1280, 640));
-//    creerRectangle(sf::)
-    creerTexte(30,sf::Color::Black,"Entrer pour jouer !",sf::Vector2f(Window.Getlargeur()/2.4,Window.Gethauteur()/50));
-    creerTexte(30,sf::Color::Black,"Echap pour quitter !",sf::Vector2f(Window.Getlargeur()/2.4,Window.Gethauteur()/1.05));
+    creerRectangle(sf::Color::Red,sf::Vector2f(0,0),sf::Vector2f(Window.Getlargeur(),64),1,sf::Color::Black);
+    creerRectangle(sf::Color::Red,sf::Vector2f(0,Window.Gethauteur()-64),sf::Vector2f(Window.Getlargeur(),64),1,sf::Color::Black);
+    creerTexte(30,sf::Color::Black,"Entrer pour jouer !",sf::Vector2f(Window.Getlargeur()/2.35,Window.Gethauteur()/70));
+    creerTexte(30,sf::Color::Black,"Echap pour quitter !",sf::Vector2f(Window.Getlargeur()/2.35,Window.Gethauteur()/1.05));
     boucle();
 }
 
